0886fa1d9e7b45c0e62b29f845452cfda35d6490
SeungYeon Hong <69149030+dev-redo@users.noreply.github.com>
2023-11-20 18:22:14 +0900

    ADCIO-1753) test: write adcio sdk test code (#10)
    * fix: Add expiration timer to SessionStorage
    
    * test: Check whether the session id has changed when expiration ends.
    
    * test: Test whether adcio core is called only once when creating an adcio instance
    
    * test: Write test code to determine whether the client id and customer id passed as params when creating an adcio instance match each field value of adcio.
    
    * style: modify test title
    
    * test: Modify adcio module test code

---
+++ src/__tests__/adcio.spec.ts
@@ -0,0 +1,76 @@
+import { AdcioCore } from "lib/core";
+import { Adcio } from "../adcio";
+
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+  jest.resetModules();
+});
+
+describe("test Adcio module", () => {
+  it("should have matching client and customer IDs", () => {
+    const clientId = "your-client-id";
+    const customerId = "your-customer-id";
+
+    const adcio = new Adcio({ clientId, customerId });
+
+    expect(adcio["config"].clientId).toBe(clientId);
+    expect(adcio["config"].customerId).toBe(customerId);
+  });
+});
+
+describe("test AdcioCore module", () => {
+  beforeEach(() => {
+    window.sessionStorage.clear();
+  });
+
+  it("should have the same session ID before expiration", async () => {
+    const adcioCore = new AdcioCore({
+      clientId: "your-client-id",
+    });
+
+    const initialSessionId = adcioCore.getSessionId();
+
+    jest.advanceTimersByTime(10000);
+
+    // Verify that the session ID remains the same after 10s
+    expect(adcioCore.getSessionId()).toBe(initialSessionId);
+  });
+
+  it("should have a different session ID after expiration", async () => {
+    const adcioCore = new AdcioCore({
+      clientId: "your-client-id",
+    });
+
+    const initialSessionId = adcioCore.getSessionId();
+
+    jest.advanceTimersByTime(30 * 60 * 1000);
+
+    // AdcioCore should reset the session ID after 30m
+    // Verify that the session ID has changed after 30m
+    expect(adcioCore.getSessionId()).not.toBe(initialSessionId);
+  });
+
+  it("should call AdcioCore constructor once when Adcio is created", async () => {
+    jest.doMock("../lib/core", () => {
+      return {
+        AdcioCore: jest.fn().mockImplementation(),
+      };
+    });
+
+    const { AdcioCore } = await import("../lib/core");
+    const { Adcio } = await import("../adcio");
+
+    new Adcio({
+      clientId: "your-client-id",
+      customerId: "your-customer-id",
+    });
+
+    expect(AdcioCore).toHaveBeenCalledTimes(1);
+
+    jest.dontMock("../lib/core");
+  });
+});
