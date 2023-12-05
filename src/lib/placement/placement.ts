994bc31405232f8c796fbfa0727e448796681dc4
dev-redo <mis05041@naver.com>
2023-12-05 11:47:47 +0900

    feat: Implement error handling using a custom error object in the createSuggestions method.

--- src/lib/placement/placement.ts
+++ src/lib/placement/placement.ts
@@ -1,5 +1,8 @@
 import { Configuration, SuggestionApi } from "api/controller/v1";
+import { isAxiosError } from "axios";
+import { ERROR_CODE } from "lib/constants/error";
 import { AdcioCore } from "lib/core";
+import { APIError } from "lib/error";
 import {
   AdcioPlacementParams,
   AdcioPlacementCreateSuggestionParams,
@@ -15,14 +18,31 @@ export class AdcioPlacement {
   }
 
   public async createSuggestion(params: AdcioPlacementCreateSuggestionParams) {
-    const { data } = await new SuggestionApi(
-      this.apiConfig,
-    ).suggestionControllerSuggest({
-      ...params,
-      sessionId: this.adcioCore.getSessionId(),
-      deviceId: this.adcioCore.getDeviceId(),
-    });
+    try {
+      const { data } = await new SuggestionApi(
+        this.apiConfig,
+      ).suggestionControllerSuggest({
+        ...params,
+        sessionId: this.adcioCore.getSessionId(),
+        deviceId: this.adcioCore.getDeviceId(),
+      });
 
-    return data;
+      return data;
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        switch (error.response.data.message) {
+          case ERROR_CODE.SUGGESTION.INVALID_PLACEMENT_TYPE:
+            throw new APIError(
+              error.response?.status,
+              `Failed to suggestions: The placement id(${params.placementId}) does not exist`,
+            );
+          default:
+            throw new APIError(
+              error.response?.status,
+              "An unknown error occurred in Adcio web sdk.",
+            );
+        }
+      }
+    }
   }
 }
