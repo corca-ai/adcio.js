8c9803c4a3e4da75f8938bc6eb8e1abaaf2ab766
dev-redo <mis05041@naver.com>
2024-02-21 00:41:19 +0900

    feat: connect impression and click event to slide

---
+++ demo/react/src/component/banner/index.tsx
@@ -0,0 +1,84 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import { Swiper as SwiperClass } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "../styles/banner.css";
+import { SuggestionDto } from "adcio.js/src/api/controller/v1/api";
+
+interface Props {
+  suggestionData: SuggestionDto[];
+  impressionSlide: (currentSlideData: SuggestionDto) => void;
+  clickSlide: (currentSlideData: SuggestionDto) => void;
+}
+
+export function Banner({ suggestionData, impressionSlide, clickSlide }: Props) {
+  const handleSlideChange = (swiper: SwiperClass) => {
+    const activeIndex = swiper.activeIndex;
+    const currentSlideData = suggestionData[activeIndex];
+    if (currentSlideData) {
+      impressionSlide(currentSlideData);
+    }
+  };
+
+  return (
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      modules={[Pagination]}
+      navigation={true}
+      className="mySwiper"
+      pagination={{ clickable: true }}
+      onSlideChange={handleSlideChange}
+    >
+      {suggestionData.map((currentSlideData) => {
+        const { banner } = currentSlideData;
+
+        return (
+          <SwiperSlide
+            key={banner.id}
+            onClick={() => clickSlide(currentSlideData)}
+          >
+            <SwiperSlide key={banner.id}>
+              <div>
+                <img src={banner.creative.mediaUrl} alt="" />
+                <div className="explain_banner">
+                  {banner.data["title"] && (
+                    <p
+                      className="title"
+                      style={{
+                        color: banner.data["titleColor"],
+                      }}
+                    >
+                      {banner.data["title"]}
+                    </p>
+                  )}
+                  {banner.data["subtitle"] && (
+                    <p
+                      className="subtitle"
+                      style={{
+                        color: banner.data["subtitleColor"],
+                      }}
+                    >
+                      {banner.data["subtitle"]}
+                    </p>
+                  )}
+                  {banner.data["description"] && (
+                    <p
+                      className="description"
+                      style={{
+                        color: banner.data["descriptionColor"],
+                      }}
+                    >
+                      {banner.data["description"]}
+                    </p>
+                  )}
+                </div>
+              </div>
+            </SwiperSlide>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+}
