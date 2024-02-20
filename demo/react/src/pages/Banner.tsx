578284b992a17f42cb5c3208b5358373e55782a1
dev-redo <mis05041@naver.com>
2024-02-20 23:46:54 +0900

    feat: implement demo

--- demo/react/src/pages/Banner.tsx
+++ demo/react/src/pages/Banner.tsx
@@ -1,50 +1,88 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "../styles/banner.css";
 
-import { Pagination } from "swiper/modules";
 import { useEffect, useState } from "react";
 import { Adcio } from "adcio.js/src/adcio";
 import { SuggestionTestId } from "../../../../mock/constants";
-import { SuggestionResponseDto } from "adcio.js/src/api/controller/v1/api";
+import { SuggestionDto } from "adcio.js/src/api/controller/v1/api";
 
 interface Props {
   adcioInstance: Adcio;
 }
 
 export default function Banner({ adcioInstance }: Props) {
-  const [data, setData] = useState<SuggestionResponseDto>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [suggestionData, setSuggestionData] = useState<SuggestionDto[]>(null);
 
   useEffect(() => {
     async function fetchMyAPI() {
       const response = await adcioInstance.createSuggestion({
         placementId: SuggestionTestId.SUCCESS_PLACEMENT,
       });
-      setData(response);
+      setSuggestionData(response.suggestions);
+      setIsLoading(false);
     }
     fetchMyAPI();
   }, []);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <Swiper
-      slidesPerView={3}
-      spaceBetween={30}
-      modules={[Pagination]}
-      className="mySwiper"
-      pagination={{
-        clickable: true,
-      }}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide>
-    </Swiper>
+    <div className="container">
+      <div className="title">[PC] Banner Test</div>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        modules={[Pagination]}
+        navigation={true}
+        className="mySwiper"
+        pagination={{
+          clickable: true,
+        }}
+      >
+        {suggestionData.map(({ banner }) => (
+          <SwiperSlide key={banner.id}>
+            <img src={banner.creative.mediaUrl} />
+            <div className="explain_banner">
+              {banner.data["title"] && (
+                <p
+                  className="title"
+                  style={{
+                    color: banner.data["titleColor"],
+                  }}
+                >
+                  {banner.data["title"]}
+                </p>
+              )}
+              {banner.data["subtitle"] && (
+                <p
+                  className="subtitle"
+                  style={{
+                    color: banner.data["subtitleColor"],
+                  }}
+                >
+                  {banner.data["subtitle"]}
+                </p>
+              )}
+              {banner.data["description"] && (
+                <p
+                  className="description"
+                  style={{
+                    color: banner.data["descriptionColor"],
+                  }}
+                >
+                  {banner.data["description"]}
+                </p>
+              )}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 }
