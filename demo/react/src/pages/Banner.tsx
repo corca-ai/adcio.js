1b7ad9698bacc38937800779b8a2fe677920eabd
dev-redo <mis05041@naver.com>
2024-02-21 00:41:19 +0900

    feat: connect impression and click event to slide

--- demo/react/src/pages/Banner.tsx
+++ demo/react/src/pages/Banner.tsx
@@ -1,21 +1,20 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "../styles/banner.css";
 
 import { useEffect, useState } from "react";
 import { Adcio } from "adcio.js/src/adcio";
-import { SuggestionTestId } from "../../../../mock/constants";
 import { SuggestionDto } from "adcio.js/src/api/controller/v1/api";
+import { SuggestionTestId } from "../../../../mock/constants";
+import { Banner } from "src/component/banner";
 
 interface Props {
   adcioInstance: Adcio;
 }
 
-export default function Banner({ adcioInstance }: Props) {
+export default function BannerPage({ adcioInstance }: Props) {
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [suggestionData, setSuggestionData] = useState<SuggestionDto[]>(null);
+  const [suggestionData, setSuggestionData] = useState<SuggestionDto[]>([]);
 
   useEffect(() => {
     async function fetchMyAPI() {
@@ -35,54 +34,15 @@ export default function Banner({ adcioInstance }: Props) {
   return (
     <div className="container">
       <div className="title">[PC] Banner Test</div>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        modules={[Pagination]}
-        navigation={true}
-        className="mySwiper"
-        pagination={{
-          clickable: true,
+      <Banner
+        suggestionData={suggestionData}
+        impressionSlide={(currentSlideData) => {
+          adcioInstance.onImpression(currentSlideData.logOptions);
         }}
-      >
-        {suggestionData.map(({ banner }) => (
-          <SwiperSlide key={banner.id}>
-            <img src={banner.creative.mediaUrl} />
-            <div className="explain_banner">
-              {banner.data["title"] && (
-                <p
-                  className="title"
-                  style={{
-                    color: banner.data["titleColor"],
-                  }}
-                >
-                  {banner.data["title"]}
-                </p>
-              )}
-              {banner.data["subtitle"] && (
-                <p
-                  className="subtitle"
-                  style={{
-                    color: banner.data["subtitleColor"],
-                  }}
-                >
-                  {banner.data["subtitle"]}
-                </p>
-              )}
-              {banner.data["description"] && (
-                <p
-                  className="description"
-                  style={{
-                    color: banner.data["descriptionColor"],
-                  }}
-                >
-                  {banner.data["description"]}
-                </p>
-              )}
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+        clickSlide={(currentSlideData) => {
+          adcioInstance.onClick(currentSlideData.logOptions);
+        }}
+      />
     </div>
   );
 }
