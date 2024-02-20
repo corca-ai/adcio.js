e11f3d72e753b9d976a8c55c994047b235d4c4a7
dev-redo <mis05041@naver.com>
2024-02-20 19:48:14 +0900

    feat: connect adcio instance suggestion

--- demo/react/src/App.tsx
+++ demo/react/src/App.tsx
@@ -1,12 +1,21 @@
 import { Navigate, Routes as ReactRouterRoutes, Route } from "react-router-dom";
 import BannerPage from "./pages/Banner";
 import StartPage from "./pages";
+import { Adcio } from "adcio.js/src/adcio";
 
 export default function App() {
+  const adcioInstance = new Adcio({
+    clientId: "your-client-id",
+    customerId: "your-customer-id",
+  });
+
   return (
     <>
       <ReactRouterRoutes>
-        <Route path="/banner" element={<BannerPage />} />
+        <Route
+          path="/banner"
+          element={<BannerPage adcioInstance={adcioInstance} />}
+        />
         <Route path="/" element={<StartPage />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </ReactRouterRoutes>
