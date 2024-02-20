479fc59ca0f3a1b032739cebe59e821e7f2146a6
dev-redo <mis05041@naver.com>
2024-02-20 22:13:56 +0900

    feat: connect suggestion mock api

--- demo/react/vite.config.ts
+++ demo/react/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      lib: path.resolve(__dirname, "../../src/lib"),
+      api: path.resolve(__dirname, "../../src/api"),
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
 });
