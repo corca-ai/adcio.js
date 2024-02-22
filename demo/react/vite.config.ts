a1cebf256e805ca041bd671ce731ebc65f7d770f
dev-redo <mis05041@naver.com>
2024-02-22 12:05:30 +0900

    fix: modified to install the published adcio.js library

--- demo/react/vite.config.ts
+++ demo/react/vite.config.ts
@@ -4,12 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      'adcio.js': path.resolve(__dirname, '../../node_modules/adcio.js'),
-      lib: path.resolve(__dirname, "../../src/lib"),
-      api: path.resolve(__dirname, "../../src/api"),
-    },
-  },
   plugins: [react(), tsconfigPaths()],
 });
