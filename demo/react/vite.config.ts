54512aa1cc1bda6752fd9bca9817503ec307665e
dev-redo <mis05041@naver.com>
2024-02-22 14:08:53 +0900

    fix: fix demo run issue

--- demo/react/vite.config.ts
+++ demo/react/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'adcio.js': path.resolve(__dirname, '../../node_modules/adcio.js'),
+      lib: path.resolve(__dirname, "../../src/lib"),
+      api: path.resolve(__dirname, "../../src/api"),
+    },
+  },
   plugins: [react(), tsconfigPaths()],
 });
