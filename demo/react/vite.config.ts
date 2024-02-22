774120c868d10c97893466c3ab4c7a0ba18184b8
dev-redo <mis05041@naver.com>
2024-02-22 11:52:53 +0900

    fix: add adcio.js path to vite config file

--- demo/react/vite.config.ts
+++ demo/react/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 export default defineConfig({
   resolve: {
     alias: {
+      'adcio.js': path.resolve(__dirname, '../../node_modules/adcio.js'),
       lib: path.resolve(__dirname, "../../src/lib"),
       api: path.resolve(__dirname, "../../src/api"),
     },
