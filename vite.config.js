bcfc4c94f86012cb326c3453abcb4db79e7a7576
dev-redo <mis05041@naver.com>
2024-03-02 23:40:56 +0900

    chore: support cjs

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
-      formats: ["es", "iife"],
+      formats: ["es", "cjs", "iife"],
       name: "adcio",
       fileName: "index",
     },
