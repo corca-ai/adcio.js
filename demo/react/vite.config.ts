9033cafaa8d700968ff8d97ab5e5852696ddb1e7
SeungYeon Hong <69149030+dev-redo@users.noreply.github.com>
2024-03-27 18:23:35 +0900

    NONE) chore: remove private option (#41)
    * chore: remove private option
    
    * chore: modify adcio version
    
    ---------
    
    Co-authored-by: dev-redo <mis05041@naver.com>

--- demo/react/vite.config.ts
+++ demo/react/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "adcio.js": path.resolve(__dirname, "../../src"),
-    },
-  },
   plugins: [react(), tsconfigPaths()],
 });
