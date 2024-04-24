9882816553b7a08fc1a1b66746e65554a910f2f9
Jinho Hyeon <neverlish@gmail.com>
2024-04-24 15:42:42 +0900

    refactor: set dependenciesMeta

--- demo/react/vite.config.ts
+++ demo/react/vite.config.ts
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["@adcio/core"],
-    }
-  },
   plugins: [react(), tsconfigPaths()],
 });
