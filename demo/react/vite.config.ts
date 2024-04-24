57097e7fd2764a5e5a86ca92414cf6752cbdd0e2
Jinho Hyeon <neverlish@gmail.com>
2024-04-24 11:52:10 +0900

    chore: demo/react rollupOption external

--- demo/react/vite.config.ts
+++ demo/react/vite.config.ts
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["@adcio.js/core"],
+    }
+  },
   plugins: [react(), tsconfigPaths()],
 });
