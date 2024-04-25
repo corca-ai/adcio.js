5b36ba1ca53b3b401e71182880209d675bb8842c
hanchchch <cndghks15@gmail.com>
2024-04-25 20:40:48 +0900

    fix: add-to-cart quantity

--- packages/core/src/adcio.ts
+++ packages/core/src/adcio.ts
@@ -173,6 +173,7 @@ export class Adcio {
       this.onAddToCart({
         cartId: cart.id,
         productIdOnStore: cart.productIdOnStore,
+        quantity: cart.quantity,
       }),
     );
   }
