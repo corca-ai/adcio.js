0e5038b814d14a40febadb70b997cd52322883c0
dev-redo <mis05041@naver.com>
2024-06-17 15:02:20 +0900

    chore: remove eslint

--- packages/agent/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: "../../.eslintrc.js",
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-  ],
-  plugins: ["react", "react-hooks", "prettier"],
-};
