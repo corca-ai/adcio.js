9e8abace163c7be33c3d3afbb47aa6af0a5f4ba5
dev-redo <mis05041@naver.com>
2024-06-17 13:30:04 +0900

    fix: fix icon issue

--- packages/agent/src/components/startPage/StartTop.tsx
+++ packages/agent/src/components/startPage/StartTop.tsx
@@ -1,28 +1,17 @@
-import { IconX } from '@tabler/icons-react';
-
-import { ActionIcon, Flex, FlexProps, createStyles } from '@mantine/core';
+import { Icon } from "@corca-ai/design-system";
+import { ActionIcon, Flex, FlexProps } from "@mantine/core";
 
 interface Props extends FlexProps {
   showClose: boolean;
   onClose: () => void;
 }
 
-const useStyles = createStyles((theme) => ({
-  button: {
-    stroke: theme.colors.gray[1],
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-}));
-
 export function StartTop({ showClose, onClose, ...props }: Props) {
-  const { classes } = useStyles();
   return (
     <Flex align="center" justify="flex-end" {...props}>
       {showClose && (
         <ActionIcon>
-          <IconX size="15" className={classes.button} onClick={onClose} />
+          <Icon.Dot size="15" onClick={onClose} />
         </ActionIcon>
       )}
     </Flex>
