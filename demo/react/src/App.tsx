312faed9dd8ceb004974afd68f7ed9458982c23e
dev-redo <mis05041@naver.com>
2024-06-19 11:19:26 +0900

    remove: remove agent info

--- demo/react/src/App.tsx
+++ demo/react/src/App.tsx
@@ -3,7 +3,6 @@ import BannerPage from "./pages/Banner";
 import StartPage from "./pages";
 import { Adcio } from "@adcio/core";
 import { renderAgent } from "@adcio/agent";
-import { createStorage } from "../../../packages/lib/storage/storage.factory";
 
 export default function App() {
   const adcioInstance = new Adcio({
@@ -13,10 +12,10 @@ export default function App() {
 
   renderAgent({
     deviceId: "id",
-    customerId: "61c2adf4-4886-4237-9a42-3da6f0ce963f",
+    customerId: "",
     agentProfile: {
-      id: "4d2e5e51-c91a-46b9-957b-3b90cddceb2c",
-      clientId: "76dc12fa-5a73-4c90-bea5-d6578f9bc606",
+      id: "",
+      clientId: "",
       imageUrl: null,
       name: "ANDAR",
       description: "",
@@ -28,7 +27,7 @@ export default function App() {
         "안녕하세요. 안다르 챗봇 추천시스템입니다. 저희는 다양한 카테고리, 성별, 사이즈, 활동복, 색상, 가격대에 따른 개인 맞춤형 추천을 해드릴 수 있습니다. 무엇을 원하시나요?",
       bottomMargin: 0,
       client: {
-        id: "76dc12fa-5a73-4c90-bea5-d6578f9bc606",
+        id: "",
         name: "ANDAR",
         currency: "KRW",
         timezone: "KST",
@@ -37,9 +36,9 @@ export default function App() {
         freeCredit: 0,
         type: "OWN_WEBSITE",
         deletedAt: null,
-        paymentCardId: "3259976c-66d9-4c46-b759-6452f872d802",
-        storeId: "284726d6-4460-4673-bd9c-49b05577fb7c",
-        businessLicenseId: "1718f116-dab2-44e1-9854-9f85783d3299",
+        paymentCardId: "",
+        storeId: "",
+        businessLicenseId: "",
         isSeller: false,
       },
     },
