1b8532898746b7d412b048f718a7ca7ffe734f92
hanchchch <cndghks15@gmail.com>
2024-09-20 18:11:23 -0700

    feat: rosemom pc

--- packages/widget/src/renderer/index.ts
+++ packages/widget/src/renderer/index.ts
@@ -1,8 +1,9 @@
-import { SimpleGridRenderer } from "./simple-grid-renderer";
 import { ArtishGridRenderer } from "./artish-grid-renderer";
 import { ArtishMoGridRenderer } from "./artish-mo-grid-renderer";
+import { RosemomGridRenderer } from "./rosemom-grid-renderer";
+import { SimpleGridRenderer } from "./simple-grid-renderer";
 
-export { SimpleGridRenderer, ArtishGridRenderer };
+export { SimpleGridRenderer, ArtishGridRenderer, RosemomGridRenderer };
 
 export const getRenderer = (id: string) => {
   switch (id) {
@@ -12,6 +13,8 @@ export const getRenderer = (id: string) => {
       return new ArtishGridRenderer();
     case "390e516a-5f97-4c93-ba50-daca4b1222b9":
       return new ArtishMoGridRenderer();
+    case "61a5bb9a-b053-4802-8bee-6b88b51b3f1d":
+      return new RosemomGridRenderer();
     default:
       throw new Error(`Renderer with id ${id} not found`);
   }
---
+++ packages/widget/src/renderer/rosemom-grid-renderer.ts
@@ -0,0 +1,28 @@
+import { Product } from "@adcio.js/api/controller/v1";
+import { GridRenderer } from "./grid-renderer";
+import item from "../assets/templates/rosemom-grid/item.html?raw";
+import list from "../assets/templates/rosemom-grid/list.html?raw";
+
+export class RosemomGridRenderer extends GridRenderer {
+  templateItem = item;
+  templateList = list;
+
+  refineProduct(product: Product) {
+    const image =
+      product.additionalImages?.find((i) =>
+        i.includes("/web/product/medium"),
+      ) || product.image;
+    const discountRate = Math.round(
+      (((product as any).detail.data.retail_price - product.price) /
+        (product as any).detail.data.retail_price) *
+        100,
+    );
+    return {
+      ...super.refineProduct(product),
+      discountRate:
+        isFinite(discountRate) && discountRate > 0 ? discountRate : null,
+      image,
+      custom: {},
+    };
+  }
+}
